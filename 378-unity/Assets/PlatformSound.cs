9a1e1288aedea0da81021d988aa88169a148059e
Brian Luu <beluu@calpoly.edu>
2015-05-25 23:54:29 -0700

    Sound scripts for landing on platform

---
+++ 378-unity/Assets/PlatformSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSound : MonoBehaviour {
+
+	public AudioClip JetEngine;
+	private AudioSource source;
+	
+	void Awake () {
+		
+		source = GetComponent<AudioSource>();
+	}
+	
+	
+	void OnCollisionEnter (Collision coll)
+	{
+		source.PlayOneShot(JetEngine, 1.0f);
+	}
+	
+}
