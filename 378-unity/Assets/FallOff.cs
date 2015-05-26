3b6b95f2aba4e301f40a108b672777472c60a2f3
Brian Luu <beluu@calpoly.edu>
2015-05-26 16:38:29 -0700

    Sound effects applied to all platforms (all new ones should have script automatically applied through prefab) Ship has a sound effect, drops off over distance (can change sound - using a temporary one) Initial set up for fall detection - will work on later

---
+++ 378-unity/Assets/FallOff.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallOff : MonoBehaviour {
+	
+	public AudioClip LoseSound;
+	private AudioSource source;
+	
+	void Awake () {
+		
+		source = GetComponent<AudioSource>();
+	}
+
+	/*
+	// Not being used anymore
+	void OnCollisionEnter (Collision coll)
+	{
+		source.PlayOneShot(LoseSound, 1.0f);
+	}
+	*/
+
+
+	void OnTriggerEnter (Collider coll)
+	{
+		source.PlayOneShot(LoseSound, 1.0f);
+	}
+}
