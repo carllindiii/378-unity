4529bf972237d6f8e9bb2a4512d0cf5483232c67
Kevin Tsui <kjtsui10@gmail.com>
2015-05-29 00:49:02 -0700

    Moving Platform and Hold Character scripts - The script holding the character to the moving platform is extremely buggy

---
+++ 378-unity/Assets/HoldCharacter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldCharacter : MonoBehaviour {
+
+	/* When the entity collides with this object, 
+	 * the entity's movement is attached to this object */
+	void OnTriggerEnter(Collider col) {
+		Debug.Log ("Entered");
+		col.transform.parent = gameObject.transform;
+	}
+
+	/* When the entity leaves this object, 
+	 * remove the attached relationship to this object */
+	void OnTriggerExit(Collider col) {
+		Debug.Log ("Exited");
+		col.transform.parent = null;
+	}
+}
---
+++ 378-unity/Assets/MovingPlatform.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	/* INSTANCE VARIABLES */
+	public Transform movingPlatform;
+	public Transform position1;
+	public Transform position2;
+	public Vector3 newPosition;
+	public string currentState;
+	public float smooth;
+	public float resetTime;
+
+	// Use this for initialization
+	void Start() {
+		ChangeTarget();
+	}
+	
+	// FixedUpdate is different from Update in the sense that FixedUpdate remains the same even during Frame Rate drop
+	void FixedUpdate() {
+		movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, smooth * Time.deltaTime);
+	}
+
+	// Change the currentState depending on currentState (if that makes sense)
+	void ChangeTarget() {
+		if (currentState == "To Position 1") {
+			currentState = "To Position 2";
+			newPosition = position2.position;
+		} else if (currentState == "To Position 2") {
+			currentState = "To Position 1";
+			newPosition = position1.position;
+		} else if (currentState == "") {
+			currentState = "To Position 2";
+			newPosition = position2.position;
+		}
+		Invoke("ChangeTarget", resetTime);
+	}
+
+}
