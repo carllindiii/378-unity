70305da60f9a77f2a386ae60effd25945158a0d9
carllindiii <carl.lind.iii@gmail.com>
2015-05-30 17:05:17 -0700

    adding super simple script to load text on NPC

--- 378-unity/Assets/DisplayText.cs
+++ 378-unity/Assets/DisplayText.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class DisplayText : MonoBehaviour {
-	
+
+	void Awake() {
+
+	}
+
 	void OnTriggerEnter(Collider enter) {
-		Debug.Log ("ENTERED ROBOT'S REALM");
+		TextMesh robotText = (TextMesh) GetComponentInChildren(typeof(TextMesh));
+		robotText.text = "Hello Human! You must reach the top of the terrain\nand claim this planet as your own! Good Luck :)";
 	}
 
 	void OnTriggerExit(Collider enter) {
-		Debug.Log ("LEAVING ROBOT'S REALM");
+		TextMesh robotText = (TextMesh) GetComponentInChildren(typeof(TextMesh));
+		robotText.text = "";
 	}
 	
 }
---
+++ 378-unity/Assets/RobotText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotText : MonoBehaviour {
+
+	public TextMesh robotText;
+
+	void Awake() {
+		robotText = (TextMesh)GetComponent(typeof(TextMesh));
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
