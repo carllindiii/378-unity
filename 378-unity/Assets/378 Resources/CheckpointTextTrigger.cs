8eed104201370d55c122f8b83d95be668c64cfc9
Brian Luu <beluu@calpoly.edu>
2015-06-10 12:24:35 -0700

    Checkpoints display text on first enter. Color/text can be changed

---
+++ 378-unity/Assets/378 Resources/CheckpointTextTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CheckpointTextTrigger : MonoBehaviour {
+
+	public Text message;
+	bool checkActivate = false;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	void OnTriggerEnter(Collider enter) {
+		if (checkActivate == false) {
+			message.text = "Checkpoint!\nPress ESC to return.";
+			checkActivate = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider enter) {
+		message.text = "";
+	}
+}
